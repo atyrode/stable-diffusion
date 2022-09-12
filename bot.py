18da7e957ad705ec2da35816a806f6341b816e14
Victor Ballu <victorballu@gmail.com>
2022-09-13 01:35:57 +0200

    archi and first steps

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
 import discord
 from dotenv import load_dotenv
 import os
+import asyncio
+
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-class MyClient(discord.Client):
+class Bot(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        
+        channel = self.get_channel(1019007726455619615)
+        await channel.send("test", file=discord.File('outputs/txt2img-samples/kitty_cat/seed_27_00000.png'))
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+    
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-intents = discord.Intents.default()
-intents.message_content = True
+    bot = Bot(intents=intents)
+    bot.run(DISCORD_TOKEN)
 
-client = MyClient(intents=intents)
-client.run(DISCORD_TOKEN)
---
+++ download_model.sh
@@ -0,0 +1,1 @@
+curl -o ./models/ldm/stable-diffusion-v1/model.ckpt --create-dirs https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media
